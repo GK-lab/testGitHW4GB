eb20554f17134705102fda2694d4886e5cc5f986
German <german-erdman@mail.ru>
2020-12-28 03:04:58 +0300

    HW4 done

--- src/lesson4/Workers.java
+++ src/lesson4/Workers.java
@@ -1,4 +1,43 @@
 package lesson4;
 
 public class Workers {
+    //1 Создать класс "Сотрудник" с полями: ФИО, должность, телефон, зарплата, возраст;
+    private String name;
+    private String position;
+    private int phoneNumber;
+    private int wage;
+    private int age;
+
+    //2 Конструктор класса должен заполнять эти поля при создании объекта;
+
+    Workers(String name,String position, int phoneNumber, int wage,int age){
+        this.name=name;
+        this.position=position;
+        this.phoneNumber=phoneNumber;
+        this.wage=wage;
+        this.age=age;
+    }
+    String getName() {
+        return name;
+    }
+    String getPosition() {
+        return position;
+    }
+    int getPhone() {
+        return phoneNumber;
+    }
+    int getWage() {
+        return wage;
+    }
+    int getAge() {
+        return age;
+    }
+
+
+    String infoWorker (){
+       return this.name + "_" + this.position + "_" +
+               this.phoneNumber + "_" + this.wage + "_" +
+               this.age;
+    }
+
 }
